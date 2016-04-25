581145d46731fc9d1a8b2a1154fb3463173599cd
alvaropeon <alvaro_peon@hotmail.com>
2016-04-25 11:32:59 -0700

    Update app.settings.js

--- app/app.settings.js
+++ app/app.settings.js
@@ -1,5 +1,5 @@
 var app = angular.module('hpsa-client');
 
 app.constant('AppSettings', {
-   apiUrl: 'http://expense-api.52.39.108.65.nip.io/',
+   apiUrl: 'http://expense-api.52.33.180.28.nip.io/',
 })
